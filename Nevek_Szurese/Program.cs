411ac8f8824a2d9c9c58375b7c9089e0aaf5a7b5
deakcs <deakcs@310-T>
2025-11-28 09:04:31 +0100

    Add project files.

---
+++ Nevek_Szurese/Program.cs
@@ -0,0 +1,10 @@
+﻿namespace Nevek_Szurese
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
