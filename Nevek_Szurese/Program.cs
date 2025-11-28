c66490b3b8b6625ecbe1341d15c75796b951e0aa
deakcs <deakcs@310-T>
2025-11-28 09:11:33 +0100

    Menü elkészítése

--- Nevek_Szurese/Program.cs
+++ Nevek_Szurese/Program.cs
@@ -4,7 +4,50 @@
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            while (true)
+            {
+                Console.Clear();
+                Console.WriteLine("0 Kilépés");
+                Console.WriteLine("1 Néveleji szóközök levágva");
+                Console.WriteLine("2 Csak nagybetűvel kezdődők");
+                Console.WriteLine("3 Csak számjegy nélküliek");
+                Console.WriteLine("4 Csak speciális jel nélküliek");
+                Console.WriteLine("5 Legalább egy szóközt tartalmazók");
+                Console.WriteLine("6 Csak rendes nevek");
+                string valaszt=Console.ReadLine();
+                if (valaszt == "0")
+                {
+                    return;
+                }
+                else if (valaszt == "1")
+                {
+                    //1
+                }
+                else if (valaszt == "2")
+                {
+                    //2
+                }
+                else if (valaszt == "3")
+                {
+                    //3
+                }
+                else if (valaszt == "4")
+                {
+                    //4
+                }
+                else if (valaszt == "5")
+                {
+                    //5
+                }
+                else if (valaszt == "6")
+                {
+                    //6
+                }
+                else {
+                    Console.WriteLine("Nincs ilyen parancs!");
+                }
+                    Console.ReadKey();
+            }
         }
     }
 }
